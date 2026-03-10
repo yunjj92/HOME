df2e1316e79ba2867124aff53ca4f88f52cd4a86
yunjj92 <yunjj92@gmail.com>
2026-03-10 15:27:55 +0900

    module-api 설정 진행 중, gradle folder 추가

---
+++ backend/module-api/src/main/java/com/homeproject/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.homeproject.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI setOpenAPIInfo(){
+        return new OpenAPI().info(new Info()
+                .title("Project HOME")
+                .description("백엔드 멀티 모듈 API 명세서")
+        .version("1.0"));
+    }
+
+}
