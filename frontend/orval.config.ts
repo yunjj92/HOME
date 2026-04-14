844fc011baa640522e188e97da620e8a4bfd3874
yunjj92 <yunjj92@gmail.com>
2026-04-14 11:51:08 +0900

    router 설정에 누락에 따른 에러 수정

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
           useQuery: true,
           useInfinite: false,
           useMutation: true,
+          useInvalidate: true,
+          signal: true,
           // ✅ 여기에 추가하세요!
           options: {
             staleTime: Infinity, // 앱이 켜져 있는 동안 데이터를 'fresh'하게 유지
