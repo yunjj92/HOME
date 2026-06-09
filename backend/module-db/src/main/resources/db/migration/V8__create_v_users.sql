2f2684c6128396f3a28e2c9f6397fe9266d991f2
yunjj92 <yunjj92@gmail.com>
2026-06-09 15:01:09 +0900

    Users 테이블의 view 생성

---
+++ backend/module-db/src/main/resources/db/migration/V8__create_v_users.sql
@@ -0,0 +1,18 @@
+create or replace view v_users as
+       select
+           id,
+           user_id,
+           credential_id,
+           public_key,
+           signature_count,
+           created_at,
+           created_by,
+           updated_at,
+           updated_by,
+           deleted_at,
+           deleted_by
+        from users
+       where
+           deleted_at is null
+       order by
+           id asc;
